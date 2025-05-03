bf28beebd3797946d81c68ded66d3fc39e0583a6
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-05-03 20:58:02 +0900

    ABC403 B

--- ABC403/B.py
+++ ABC403/B.py
@@ -1,14 +1,28 @@
 def main():
     n = int(input())
     li = list(map(int, input().split()))
-    
-    ans = [0] * (len(li))
-    index = 1
-    for i in range(100,1,-1):
-        for j in range(len(li)):
-            if li[j] == i:
-                ans[j] = index
 
-    print(ans)
+    indexedScores = [(score, i) for i, score in enumerate(li)]
+
+    indexedScores.sort(reverse=True)
+
+    ranks = [0] * n
+    r = 1; i = 0
+
+    while i < n:
+        currentScore = indexedScores[i][0]
+        j = i
+        while j < n and indexedScores[j][0] == currentScore:
+            j += 1
+
+        for k in range(i,j):
+            personIndex = indexedScores[k][1]
+            ranks[personIndex] = r
+        
+    r += (j - i)
+    i = j
+
+    for rank in ranks:
+        print(rank)
 
 main()
