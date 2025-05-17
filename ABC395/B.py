fb318c1cbb0d1011a0c706c50bec3fbfae1b7090
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-05-17 12:02:22 +0900

    395_B　復習

--- ABC395/B.py
+++ ABC395/B.py
@@ -1,6 +1,26 @@
 def main():
     n = int(input())
-
+    li = [["." for _ in range(n)] for _ in range(n)]
     
+    for i in range(1,n+1):
+        j = n + 1 - i
+        if i > j: continue
+
+        color = "#" if i % 2 == 1 else "."
+
+        for x in range(i-1,j):
+            for y in range(i-1,j):
+                li[x][y] = color
+    printList(li)
+    
+def printList(l):
+    for i in range(len(l)):
+        s = ""
+        for e in l[i]:
+            s += e
+        print(s)
 
 main()
+
+#はじめに N*N を作る -> 編集
+# 短形領域を塗りつぶす <- !
