78986bf121a271d1f4c0fb2693f382cfb3d554fc
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-07-07 21:33:11 +0900

    ABC413 D振り返り

--- ABC413/D.py
+++ ABC413/D.py
@@ -2,20 +2,48 @@ def main():
     T =int(input())
     N = []
     LA = []
+    LB = []
     for _ in range(T):
         n = int(input())
         A = sorted(list(map(int,input().split())))
-        N.append(n); LA.append(A)
+        B = sorted(A,key=abs)
+        N.append(n); LA.append(A); LB.append(B)
 
     for i in range(T):
-        n = N[i]; A = LA[i]
-        k = 0
-        mid = len(A)//2; pre = A[0] * A[-1]
-        while k < mid+1:
-            if A[k] * A[len(A)-(k+1)] != pre:
-                print("No")
+        n = N[i]; A = LA[i]; B = LB[i]
+
+        #通常ソート
+        for i in range(n-2):
+            if not A[i] * A[i+2] == A[i+1]**2:
                 break
-            k += 1
-        else: print("Yes")
+        else:
+            print("Yes")
+            continue
+
+        #絶対値ソート
+        for i in range(n-2):
+            if not B[i] * B[i+2] == B[i+1]**2:
+                break
+        else:
+            print("Yes")
+            continue
+        
+        # コーナーケース対応
+        if abs(A[0]) == abs(A[-1]):
+            x = 0; y = 0
+            for i in range(n):
+                if A[i] == A[0]: x += 1
+                elif A[i] == A[-1]: y += 1
+                else: break
+            else:
+                if abs(x - y) <= 1:
+                    print("Yes")
+                    continue
+            print("No")
+        else:
+            print("No")
 
 main()
+#通常ソート
+#絶対値ソート
+#コーナーケース対応
